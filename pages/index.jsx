81b17876678ba45e4199f53d2f3ae503bba5d86b
Oancea Alin <74875548+Oanee@users.noreply.github.com>
2022-11-06 18:45:26 +0200

    monochrome

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <button className="button button-primary">Button Primary</button>
+      <button className="button button-primary button-small">
+        Button Primary Small
+      </button>
+      <a className="button button-primary">Button Primary</a>
+      <a className="button button-primary button-small">Button Primary Large</a>
+    </div>
+  );
+}
