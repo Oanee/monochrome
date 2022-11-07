f8fff3862ac56da3d438c8c51e10166db9e0d7f8
Oancea Alin <74875548+Oanee@users.noreply.github.com>
2022-11-07 19:07:25 +0200

    monochrome buttons

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,8 +5,16 @@ export default function Home() {
       <button className="button button-primary button-small">
         Button Primary Small
       </button>
-      <a className="button button-primary">Button Primary</a>
-      <a className="button button-primary button-small">Button Primary Large</a>
+      <button className="button button-primary button-large">
+        Button Primary large white
+      </button>
+      <button className="button button-primary button-large button-black">
+        Button Primary large black
+      </button>
+      <button className="button button-primary button-mobile">
+        Button Primary mobile
+      </button>
+      <a className="button button-primary">Button Primary anchor</a>
     </div>
   );
 }
