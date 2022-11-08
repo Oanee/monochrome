2d181ae6fd8f7d7ed9cb1cc7ec92044823026a56
Oancea Alin <74875548+Oanee@users.noreply.github.com>
2022-11-08 21:02:24 +0200

    monochrome button 100%

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,16 @@
 export default function Home() {
   return (
     <div>
-      <button className="button button-primary">Button Primary</button>
-      <button className="button button-primary button-small">
-        Button Primary Small
+      <button className="button">view all</button>
+      <button className="button button-uppercase button-small">submit</button>
+      <button className="button button-uppercase button-large">
+        $ high to low
       </button>
-      <button className="button button-primary button-large">
-        Button Primary large white
+      <button className="button button-uppercase button-large button-black">
+        bestsellers
       </button>
-      <button className="button button-primary button-large button-black">
-        Button Primary large black
-      </button>
-      <button className="button button-primary button-mobile">
-        Button Primary mobile
-      </button>
-      <a className="button button-primary">Button Primary anchor</a>
+      <button className="button button-uppercase button-mobile">new in</button>
+      <a className="button button-anchor">view all</a>
     </div>
   );
 }
