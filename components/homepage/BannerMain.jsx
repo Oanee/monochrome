c086db6e0105bbacb7832bd9247c811eb82cbbb1
Oancea Alin <74875548+Oanee@users.noreply.github.com>
2022-11-19 15:25:25 +0200

    header 100%

--- components/homepage/BannerMain.jsx
+++ components/homepage/BannerMain.jsx
@@ -4,7 +4,7 @@ export default function BannerMain() {
   return (
     <>
       <div className="banner-main d-lg-none">
-        <div className="container">
+        <div className="banner-main-inner container">
           <div className="button button-uppercase button-small banner-cta">
             <Link href="/" title="New in">
               New in
